7984d2eec9ed1fcb4c36853000fc2916d6ee5056
Harish <harishanbazhagan.s@gmail.com>
2025-07-27 16:09:41 +0530

    add question for SubsetsII
    Signed-off-by: Harish <harishanbazhagan.s@gmail.com>

---
+++ src/Recursion/Advanced/SubsetsII.java
@@ -0,0 +1,13 @@
+package Recursion.Advanced;
+
+/*
+Given an integer array nums, which can have duplicate entries, provide the power set.
+Duplicate subsets cannot exist in the solution set.
+Return the answer in any sequence.
+
+example:
+Input : nums = [1, 2, 2]
+Output : [ [ ] , [1] , [1, 2] , [1, 2, 2] , [2] , [2, 2] ]
+ */
+public class SubsetsII {
+}
